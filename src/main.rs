8660ac6dded1e6ef3b22023b592df59cccd6c176
dastansam <dastan.samatov.33@gmail.com>
2025-01-14 23:19:38 +0600

    Add binary tree

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::{
     vec,
 };
 
+mod binary_tree;
+
 /// A program which solves the "word chain game". A word chain is a collection of words created by mutating one letter from the previous one, e.g. cat -> sat -> sit.
 /// In the word chain game you are given two words and asked to find the length of the shortest word chain which connects them (if one exists).
 /// As input we are given a list of words along with the two to find the chain between. We assume all words in the given list are the same length, and are only made up of ASCII characters.
